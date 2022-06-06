823838b7f3e161c58332f2c11520d580a65d49b1
Jobsidney <jobsidney67@gmail.com>
2022-06-06 14:41:51 +0300

    commit tree 10f7dda96a61bcf839f45cc065af57c08b328b97

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  return distance<=400?"This one is on me!"
+  :distance<2400?"I will gladly take your thirty bucks."
+  :"No can do.";
+    
+  }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city==="NYC"?"Ok, sounds good.":"No go."
+}
+let tip="generous"
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (true) {
+    case tip==="generous":
+      return "Thank you so much.";
+      break;
+    case tip==="not as generous":
+      return "Thank you."
+      break;
+    default: 
+      return "Bye.";
+
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
